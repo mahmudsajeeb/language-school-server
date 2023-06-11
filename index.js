6447439c0c4a0cfab118d78881685f5a496d28d4
Mahmud sajib <mahmudsajib082@gmail.com>
2023-06-11 13:16:12 +0600

    delete

--- index.js
+++ index.js
@@ -83,7 +83,14 @@ async function run() {
           res.send(result)
         })
        
-        
+         app.delete('/books/:id', async (req, res) => {
+          const id = req.params.id;
+           
+          const query = { _id: new ObjectId(id) };
+          const result = await booksCollection.deleteOne(query);
+          console.log(result)
+          res.send(result);
+    })
   
      
     // Send a ping to confirm a successful connection
